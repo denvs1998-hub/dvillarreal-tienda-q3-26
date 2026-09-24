53bc88eac2e40ebe466ab819d53e3c285df84d12
denvs1998-hub <denvs1998@gmail.com>
2026-09-23 19:01:36 -0600

    techShop - página HTML

---
+++ techShop/src/test/java/techShop/techShop/TechShopApplicationTests.java
@@ -0,0 +1,13 @@
+package techShop.techShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
